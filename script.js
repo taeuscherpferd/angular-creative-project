2bd18878474cbc2273349de44ea8267b8d36ee60
taeuscherpferd <ibber@myokcorral.com>
2018-10-15 17:23:27 -0600

    got the request working

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 var app=angular.module("myApp",[]);
 app.controller('myCtrl', 
   function($scope, $http) {
+
     $scope.onup = function(form) {
+      var config = {
+          headers: {
+              'Accept': 'application/json'
+          }
+      };
       console.log(form);
 
       var url = "https://icanhazdadjoke.com/";
-      $http.get(url).then(function(response) {
+      $http.get(url, config).then(function(response) {
         console.log(response);
-        $scope.resopnse = response.data;
+        $scope.returnData = response.data;
       }); 
+      // $http({
+      //   METHOD:"GET",
+      //   url:url,
+      //   headers:{
+      //     'Accept':'application/json'
+      //   }
+      // }).then(function(res){
+      //   console.log(res);
+      // })
     }   
   }) 
