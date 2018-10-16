ad0969ec199b9522bc842515917c52f08dde079f
Brandon <bjorgen3@byu.edu>
2018-10-16 04:59:01 +0000

    added functionality, start on formatting

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 var app=angular.module("myApp",[]);
 app.controller('myCtrl', 
   function($scope, $http) {
-
+    
     $scope.onup = function(form) {
+      $scope.jokes = [];
       var config = {
           headers: {
               'Accept': 'application/json'
           }
       };
       console.log(form);
-
-      var url = "https://icanhazdadjoke.com/";
+      if(form != "") {
+      var url = "https://icanhazdadjoke.com/search?term=" + form;
       $http.get(url, config).then(function(response) {
         console.log(response);
-        $scope.returnData = response.data;
+       // $scope.returnData = response.data;
+        $scope.results = response.data.results;
+       for (var key in $scope.results) {
+       if ($scope.results.hasOwnProperty(key)) {
+          console.log(($scope.results[key].joke));
+          $scope.jokes.push(($scope.results[key].joke));
+       }
+    }
       }); 
       // $http({
       //   METHOD:"GET",
@@ -24,5 +32,19 @@ app.controller('myCtrl',
       // }).then(function(res){
       //   console.log(res);
       // })
-    }   
+    }  
+    }
+    $scope.onclick = function(button) {
+      var config = {
+          headers: {
+              'Accept': 'application/json'
+          }
+      };
+
+      var url = "https://icanhazdadjoke.com/";
+      $http.get(url, config).then(function(response) {
+        console.log(response);
+        $scope.randomJoke = response.data.joke;
+      }); 
+    } 
   }) 
